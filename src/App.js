1f2d9195c9048a95c8072febd73f2162c57313a6
Mohan.Seelamaneni <mohan.seelamneni@gmail.com>
2020-09-10 12:22:34 +0530

    adding description about me

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     <div>
+     <h1>good evening to all</h1>
+     <h1>my self mohan krishna seelamaneni, i completed my graduation in the year of 2018, even though i completed my graduation nothing is change in my life(no job and full of mental tensions)</h1>
+     </div>
      
-      <h1>good evening to all</h1>
+
     </div>
   );
 }
